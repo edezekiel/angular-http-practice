4df2cb37e0daa724a48d22d18ef487e498e4620a
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2021-06-08 10:53:50 -0500

    Fetch and Post data using firebase database

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,21 +8,39 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent implements OnInit {
   loadedPosts = [];
+  FIREBASE_URL = 'https://ng-complete-guide-15ad4-default-rtdb.firebaseio.com/';
+  POSTS_URL = `${this.FIREBASE_URL}posts.json`;
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this._fetchPosts()
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
-    // Send Http request
-    console.log(postData);
+    this.http.post(this.POSTS_URL, postData)
+      .subscribe(data => this.loadedPosts.push(data));
   }
 
   onFetchPosts() {
-    // Send Http request
   }
 
   onClearPosts() {
-    // Send Http request
+  }
+
+  private _fetchPosts() {
+    this.http.get(this.POSTS_URL)
+      .pipe(map(resp => this._convertFireBaseRespToPosts(resp)))
+      .subscribe(posts => this.loadedPosts = posts);
+  }
+
+  private _convertFireBaseRespToPosts(resp: any) {
+    const postsArray = [];
+    for (const key in resp) {
+      if (resp.hasOwnProperty(key)) {
+        postsArray.push({ ...resp[key], id: key });
+      }
+    };
+    return postsArray;
   }
 }
